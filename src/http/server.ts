ec6c8179985410901826396d19d2864bd6b5704c
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-14 20:47:23 -0300

    feat: add route to create a restaurant

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,35 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
+import { db } from '../db/connection'
+import { restaurants, users } from '../db/schema'
 import { ENV } from '../env'
 
-const app = new Elysia().get('/', () => {
-  return 'Hello World'
-})
+const app = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({ email, name: managerName, phone })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      managerId: manager.id,
+      name: restaurantName,
+    })
+
+    set.status = 'No Content'
+  },
+  {
+    body: t.Object({
+      email: t.String({ format: 'email' }),
+      managerName: t.String({ minLength: 1 }),
+      phone: t.Optional(t.String({ minLength: 1 })),
+      restaurantName: t.String({ minLength: 1 }),
+    }),
+  },
+)
 
 console.log(process.env.NODE_ENV)
 

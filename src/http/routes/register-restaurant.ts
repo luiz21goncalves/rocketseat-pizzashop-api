c9dde5baa0bf4f02f9fef1aac568a006eaeec619
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-15 07:41:26 -0300

    refactor: add a file to register restaurant route

---
+++ src/http/routes/register-restaurant.ts
@@ -0,0 +1,31 @@
+import Elysia, { t } from 'elysia'
+
+import { db } from '../../db/connection'
+import { restaurants, users } from '../../db/schema'
+
+export const registerRestaurant = new Elysia().post(
+  '/restaurants',
+  async ({ body, set }) => {
+    const { restaurantName, managerName, email, phone } = body
+
+    const [manager] = await db
+      .insert(users)
+      .values({ email, name: managerName, phone })
+      .returning({ id: users.id })
+
+    await db.insert(restaurants).values({
+      managerId: manager.id,
+      name: restaurantName,
+    })
+
+    set.status = 'No Content'
+  },
+  {
+    body: t.Object({
+      email: t.String({ format: 'email' }),
+      managerName: t.String({ minLength: 1 }),
+      phone: t.Optional(t.String({ minLength: 1 })),
+      restaurantName: t.String({ minLength: 1 }),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,10 @@
 import cors from '@elysiajs/cors'
 import { swagger } from '@elysiajs/swagger'
-import { Elysia, t } from 'elysia'
+import { Elysia } from 'elysia'
 import { Logestic } from 'logestic'
 
-import { db } from '../db/connection'
-import { restaurants, users } from '../db/schema'
 import { ENV } from '../env'
+import { registerRestaurant } from './routes/register-restaurant'
 
 const app = new Elysia()
   .use(Logestic.preset('fancy'))
@@ -17,32 +16,7 @@ const app = new Elysia()
       provider: 'swagger-ui',
     }),
   )
-  .post(
-    '/restaurants',
-    async ({ body, set }) => {
-      const { restaurantName, managerName, email, phone } = body
-
-      const [manager] = await db
-        .insert(users)
-        .values({ email, name: managerName, phone })
-        .returning({ id: users.id })
-
-      await db.insert(restaurants).values({
-        managerId: manager.id,
-        name: restaurantName,
-      })
-
-      set.status = 'No Content'
-    },
-    {
-      body: t.Object({
-        email: t.String({ format: 'email' }),
-        managerName: t.String({ minLength: 1 }),
-        phone: t.Optional(t.String({ minLength: 1 })),
-        restaurantName: t.String({ minLength: 1 }),
-      }),
-    },
-  )
+  .use(registerRestaurant)
 
 app.listen(ENV.PORT, (server) => {
   console.log(`HTTP server running at: ${server.url}`)

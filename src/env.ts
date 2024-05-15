3afb919a1a6d67c2a921bbb6abe7457e144dea36
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-15 12:39:45 -0300

    chore: add send auth link route

--- src/env.ts
+++ src/env.ts
@@ -2,6 +2,8 @@ import z from 'zod'
 
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
+  API_BASE_URL: z.string().url(),
+  AUTH_REDIRECT_URL: z.string().url(),
   PORT: z.coerce.number(),
 })
 
--- src/http/server.ts
+++ src/http/server.ts
@@ -5,6 +5,7 @@ import { Logestic } from 'logestic'
 
 import { ENV } from '../env'
 import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
 const app = new Elysia()
   .use(Logestic.preset('fancy'))
@@ -13,10 +14,11 @@ const app = new Elysia()
     swagger({
       exclude: ['/docs', '/docs/json'],
       path: '/docs',
-      provider: 'swagger-ui',
+      provider: 'scalar',
     }),
   )
   .use(registerRestaurant)
+  .use(sendAuthLink)
 
 app.listen(ENV.PORT, (server) => {
   console.log(`HTTP server running at: ${server.url}`)

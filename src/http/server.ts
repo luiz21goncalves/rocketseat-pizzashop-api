ec26d40064e45f1defc1776f57d3ae2052746d90
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-14 21:01:56 -0300

    chore: add swagger and cors config

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,37 +1,40 @@
+import cors from '@elysiajs/cors'
+import { swagger } from '@elysiajs/swagger'
 import { Elysia, t } from 'elysia'
 
 import { db } from '../db/connection'
 import { restaurants, users } from '../db/schema'
 import { ENV } from '../env'
 
-const app = new Elysia().post(
-  '/restaurants',
-  async ({ body, set }) => {
-    const { restaurantName, managerName, email, phone } = body
+const app = new Elysia()
+  .use(cors())
+  .use(swagger({ path: '/docs', provider: 'swagger-ui' }))
+  .post(
+    '/restaurants',
+    async ({ body, set }) => {
+      const { restaurantName, managerName, email, phone } = body
 
-    const [manager] = await db
-      .insert(users)
-      .values({ email, name: managerName, phone })
-      .returning({ id: users.id })
+      const [manager] = await db
+        .insert(users)
+        .values({ email, name: managerName, phone })
+        .returning({ id: users.id })
 
-    await db.insert(restaurants).values({
-      managerId: manager.id,
-      name: restaurantName,
-    })
+      await db.insert(restaurants).values({
+        managerId: manager.id,
+        name: restaurantName,
+      })
 
-    set.status = 'No Content'
-  },
-  {
-    body: t.Object({
-      email: t.String({ format: 'email' }),
-      managerName: t.String({ minLength: 1 }),
-      phone: t.Optional(t.String({ minLength: 1 })),
-      restaurantName: t.String({ minLength: 1 }),
-    }),
-  },
-)
-
-console.log(process.env.NODE_ENV)
+      set.status = 'No Content'
+    },
+    {
+      body: t.Object({
+        email: t.String({ format: 'email' }),
+        managerName: t.String({ minLength: 1 }),
+        phone: t.Optional(t.String({ minLength: 1 })),
+        restaurantName: t.String({ minLength: 1 }),
+      }),
+    },
+  )
 
 app.listen(ENV.PORT, (server) => {
   console.log(`HTTP server running at: ${server.url}`)

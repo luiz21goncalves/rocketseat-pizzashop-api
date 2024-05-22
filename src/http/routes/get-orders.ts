ef0a2b5471298c2f9158efc4922004209ef71427
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-22 08:05:51 -0300

    feat: sort orders list

--- src/http/routes/get-orders.ts
+++ src/http/routes/get-orders.ts
@@ -1,4 +1,4 @@
-import { and, count, eq, getTableColumns, ilike } from 'drizzle-orm'
+import { and, count, desc, eq, ilike, sql } from 'drizzle-orm'
 import { createSelectSchema } from 'drizzle-typebox'
 import Elysia, { t } from 'elysia'
 
@@ -18,10 +18,14 @@ export const getOrders = new Elysia().use(auth).get(
       throw new UnauthorizedError()
     }
 
-    const orderTableColumns = getTableColumns(orders)
-
     const baseQuery = db
-      .select(orderTableColumns)
+      .select({
+        orderId: orders.id,
+        createdAt: orders.createdAt,
+        status: orders.status,
+        totalInCents: orders.totalInCents,
+        customerName: users.name,
+      })
       .from(orders)
       .innerJoin(users, eq(users.id, orders.customerId))
       .where(
@@ -41,7 +45,19 @@ export const getOrders = new Elysia().use(auth).get(
         .select()
         .from(baseQuery.as('baseQuery'))
         .offset(pageIndex * ITEMS_PER_PAGE)
-        .limit(ITEMS_PER_PAGE),
+        .limit(ITEMS_PER_PAGE)
+        .orderBy((fields) => {
+          return [
+            sql`CASE ${fields.status}
+              WHEN 'pending' THEN 1
+              WHEN 'processing' THEN 2
+              WHEN 'delivering' THEN 3
+              WHEN 'delivered' THEN 4
+              WHEN 'canceled' THEN 99
+            END`,
+            desc(fields.createdAt),
+          ]
+        }),
     ])
 
     return {

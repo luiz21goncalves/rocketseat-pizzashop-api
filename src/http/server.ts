883c914c45d0deeb8c22147cad8cd8f5bb66d322
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2024-05-23 08:09:48 -0300

    feat: add route to get day orders amount

--- src/http/server.ts
+++ src/http/server.ts
@@ -10,6 +10,7 @@ import { authenticateFromLink } from './routes/authenticate-from-link'
 import { cancelOrder } from './routes/cancel-order'
 import { deliverOrder } from './routes/deliver-order'
 import { dispatchOrder } from './routes/dispatch-order'
+import { getDayOrdersAmount } from './routes/get-day-orders-amount'
 import { getManagedRestaurant } from './routes/get-managed-restaurant'
 import { getMonthRevenue } from './routes/get-month-revenue'
 import { getOrderDetails } from './routes/get-order-details'
@@ -75,6 +76,7 @@ const app = new Elysia()
   .use(deliverOrder)
   .use(dispatchOrder)
   .use(getMonthRevenue)
+  .use(getDayOrdersAmount)
 
 app.listen(ENV.PORT, (server) => {
   console.log(`HTTP server running at: ${server.url}`)
